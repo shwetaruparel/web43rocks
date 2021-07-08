66de6f62a89eb7c81f253b85f4137bb0bcd9a366
Shweta Ruparel <70835067+shwetaruparelVP@users.noreply.github.com>
2021-07-08 11:44:15 -0700

    wip

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+
 require('dotenv').config()
+const cors = require('cors')
+const path = require('path')
 const express = require('express')
 const server = express()
-const PORT = 5000   // process.env.PORT
+
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname,'client/')))
+
+console.log(process.env.USERDOMAIN)
+const PORT = process.env.PORT || 5000  
+server.get('/',(req,res)=>{
+    console.log()
+    res.sendFile(path.join(__dirname,'client/build','index.html'))
+}) 
 server.get('/api',(req,res)=>{
     res.json({message:'web43 is working and ready to serve'})
+})
+server.use((req,res)=>{
+    res.json({message:'sorry could not serve'})
 })
 server.listen(5000,()=>{
     console.log(`I am listening at ${PORT}`)
