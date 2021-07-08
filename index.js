c2e218de2857a83ecf7a941ca8a2be7c3dd1b695
Shweta Ruparel <70835067+shwetaruparelVP@users.noreply.github.com>
2021-07-08 12:30:51 -0700

    changed the path

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const server = express()
 
 server.use(express.json())
 server.use(cors())
-server.use(express.static(path.join(__dirname,'client/')))
+server.use(express.static(path.join(__dirname,'client/build')))
 
 console.log(process.env.USERDOMAIN)
 const PORT = process.env.PORT || 5000  
 server.get('/',(req,res)=>{
-    console.log()
+    console.log("hello")
     res.sendFile(path.join(__dirname,'client/build','index.html'))
 }) 
 server.get('/api',(req,res)=>{
